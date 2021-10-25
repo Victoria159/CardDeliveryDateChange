6f8cae73fcb76ee2a37a48e83767d0b5b75877a8
Victoria159 <v.s.bocha@gmail.com>
2021-10-25 19:45:44 +0300

    Changed DataGenerator.java

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -58,10 +58,5 @@ public class CardDeliveryTest {
 
         planSuccessElement.shouldHave(text("Встреча успешно запланирована на")).shouldHave(text(date));
 
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
     }
 }
--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -23,12 +23,10 @@ public class DataGenerator {
         String[] customerName = faker.name().name().split(" ");
         return new DataGeneratorForm(cityName,
                 customerName[0] + " " + customerName[1],
-                faker.phoneNumber().cellPhone());
+                faker.phoneNumber().phoneNumber());
     }
 
     public static String getDate(int addDays) {
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
         LocalDate date = LocalDate.now().plusDays(addDays);
         return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
